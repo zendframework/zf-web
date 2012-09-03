3a18cf870955e4ba637c402f02a65b35e9df0d2e
Matthew Weier O'Phinney <matthew@zend.com>
2012-09-03 11:24:58 -0500

    Working routes and styles
    - Fixed CSS so the page looks "ok"
    - Simplified routes

--- module/Downloads/config/module.config.php
+++ module/Downloads/config/module.config.php
@@ -168,50 +168,14 @@ return array(
             'downloads' => array(
                 'type' => 'Segment',
                 'options' => array(
-                    'route'    => '/downloads[/]',
+                    'route'    => '/downloads[/[:action]]',
+                    'constraints' => array(
+                        'action' => '(latest|archives|phpcloud|skeleton-app)',
+                    ),
                     'defaults' => array(
                         '__NAMESPACE__' => 'Downloads\Controller',
                         'controller'    => 'Downloads',
                         'action'        => 'index',
-                    ),
-                    'may_terminate' => true,
-                    'child_routes'  => array(
-                        'latest' => array(
-                            'type' => 'Segment',
-                            'options' => array(
-                                'route'    => 'latest[/]',
-                                'defaults' => array(
-                                    'action' => 'latest',
-                                ),
-                            ),
-                        ),
-                        'archives' => array(
-                            'type' => 'Segment',
-                            'options' => array(
-                                'route'    => 'archives[/]',
-                                'defaults' => array(
-                                    'action' => 'archives',
-                                ),
-                            ),
-                        ),
-                        'skeleton' => array(
-                            'type' => 'Segment',
-                            'options' => array(
-                                'route'    => 'skeleton[/]',
-                                'defaults' => array(
-                                    'action' => 'skeleton-app',
-                                ),
-                            ),
-                        ),
-                        'phpcloud' => array(
-                            'type' => 'Segment',
-                            'options' => array(
-                                'route'    => 'phpcloud[/]',
-                                'defaults' => array(
-                                    'action' => 'phpcloud',
-                                ),
-                            ),
-                        ),
                     ),
                 ),
             ),
