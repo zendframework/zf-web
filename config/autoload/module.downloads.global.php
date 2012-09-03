d9cd6abbd5509238749653ae8eed9d5ada319166
Matthew Weier O'Phinney <matthew@zend.com>
2012-09-03 12:35:49 -0500

    Basic layout for downloads complete
    - Includes ZF1/ZF2 downloads
    - Styling is "ok"
    - TODO: infocard, amf, and gdata downloads
    - TODO: documentation downloads

---
+++ config/autoload/module.downloads.global.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+    'downloads' => array(
+        'versions' => array(
+            '2.0.0'    => '2012-09-06',
+            '2.0.0rc7' => '2012-08-31',
+        ),
+    ),
+);
