69e14ee8c5ca0673fbf74f8cf6119349e6cda595
Matthew Weier O'Phinney <matthew@zend.com>
2012-09-03 10:30:14 -0500

    Added downloads controller
    - Created downloads controller with three actions, index (landing page),
      latest, and archives.
    - Created factories in Module for both the release model and downloads
      controller (latter uses the former).

---
+++ module/Downloads/src/Downloads/Controller/DownloadsController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Downloads\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+
+class DownloadsController extends AbstractActionController
+{
+    protected $releases;
+
+    /**
+     * Set the release model instance
+     * 
+     * @param  ReleaseModel $model 
+     */
+    public function setReleasesModel(ReleaseModel $model)
+    {
+        $this->releases = $model;
+    }
+
+    /**
+     * downloads landing page
+     */
+    public function indexAction()
+    {
+        return null;
+    }
+
+    /**
+     * latest releases landing page
+     */
+    public function latestAction()
+    {
+        return array(
+            'releases' => $this->releases,
+        );
+    }
+
+    /**
+     * archived releases landing page
+     */
+    public function archivesAction()
+    {
+        return array(
+            'releases' => $this->releases,
+        );
+    }
+}
