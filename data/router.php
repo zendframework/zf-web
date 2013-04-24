eec6823c78040bb5188e7882b519fa3b3edd215b
Matthew Weier O'Phinney <matthew@zend.com>
2013-04-24 15:37:35 -0500

    Removed tools used in static issue page generation
    - various scripts used to create issue lists and static pages
    - template for issues
    - changes to composer.json (removed markdown, mustache)
    - removed issue data files

--- data/router.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (!preg_match('#/issues/browse/(ZF\d?-\d+)$#', $_SERVER['REQUEST_URI'], $matches)) {
-    return false;
-}
-
-echo include(__DIR__ . '/issues/browse/' . $matches[1] . '.phtml');
