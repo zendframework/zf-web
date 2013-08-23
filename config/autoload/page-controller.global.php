737db83c85a2902f3c901ace9c2b1db3e967be9d
Frank <dev@froschdesignstudio.de>
2013-08-23 12:38:07 +0200

    Add page for Zend_Config_XML-Namespace
    See #zendframework/zf1/38

--- config/autoload/page-controller.global.php
+++ config/autoload/page-controller.global.php
@@ -2,9 +2,9 @@
 return array(
     'router' => array(
         'routes' => array(
-            'participate' => array(
-                'type' => 'Zend\Mvc\Router\Http\Segment',
-                'options' => array(
+            'participate'     => array(
+                'type'          => 'Segment',
+                'options'       => array(
                     'route'    => '/participate[/]',
                     'defaults' => array(
                         '__NAMESPACE__' => 'PageController\Controller',
@@ -14,82 +14,82 @@ return array(
                 ),
                 'may_terminate' => true,
                 'child_routes'  => array(
-                    'contribute' => array(
-                        'type' => 'Zend\Mvc\Router\Http\Segment',
-                        'options' => array(
+                    'contribute'    => array(
+                        'type'          => 'Segment',
+                        'options'       => array(
                             'route'    => 'contributor-guide[/]',
                             'defaults' => array(
-                                'page'          => 'participate/contributor-guide',
+                                'page' => 'participate/contributor-guide',
                             ),
                         ),
                         'may_terminate' => true,
                     ),
                     'contribute-v1' => array(
-                        'type' => 'Zend\Mvc\Router\Http\Segment',
-                        'options' => array(
+                        'type'          => 'Segment',
+                        'options'       => array(
                             'route'    => 'contributor-guide-v1[/]',
                             'defaults' => array(
-                                'page'          => 'participate/contributor-guide-v1',
+                                'page' => 'participate/contributor-guide-v1',
                             ),
                         ),
                         'may_terminate' => true,
                     ),
-                    'conferences' => array(
-                        'type' => 'Zend\Mvc\Router\Http\Segment',
-                        'options' => array(
+                    'conferences'   => array(
+                        'type'          => 'Segment',
+                        'options'       => array(
                             'route'    => 'conferences[/]',
                             'defaults' => array(
-                                'page'          => 'participate/conferences',
+                                'page' => 'participate/conferences',
                             ),
                         ),
                         'may_terminate' => true,
                     ),
-                    'user-groups' => array(
-                        'type' => 'Zend\Mvc\Router\Http\Segment',
-                        'options' => array(
+                    'user-groups'   => array(
+                        'type'          => 'Segment',
+                        'options'       => array(
                             'route'    => 'user-groups[/]',
                             'defaults' => array(
-                                'page'          => 'participate/user-groups',
+                                'page' => 'participate/user-groups',
                             ),
                         ),
                         'may_terminate' => true,
                     ),
-                    'blogs' => array(
-                        'type' => 'Zend\Mvc\Router\Http\Segment',
-                        'options' => array(
+                    'blogs'         => array(
+                        'type'          => 'Segment',
+                        'options'       => array(
                             'route'    => 'blogs[/]',
                             'defaults' => array(
-                                'page'          => 'participate/blogs',
+                                'page' => 'participate/blogs',
                             ),
                         ),
                         'may_terminate' => true,
                     ),
-                    'contributors' => array(
-                        'type' => 'Zend\Mvc\Router\Http\Segment',
-                        'options' => array(
+                    'contributors'  => array(
+                        'type'          => 'Segment',
+                        'options'       => array(
                             'route'    => 'contributors[/]',
                             'defaults' => array(
-                                'page'          => 'participate/contributors',
+                                'page' => 'participate/contributors',
                             ),
                         ),
                         'may_terminate' => true,
                     ),
-                    'logos' => array(
-                        'type' => 'Zend\Mvc\Router\Http\Segment',
-                        'options' => array(
+                    'logos'         => array(
+                        'type'          => 'Segment',
+                        'options'       => array(
                             'route'    => 'logos[/]',
                             'defaults' => array(
-                                'page'          => 'participate/logos',
+                                'page' => 'participate/logos',
                             ),
                         ),
                         'may_terminate' => true,
                     ),
                 ),
             ),
-            'irc' => array(
-                'type' => 'Segment',
+            'irc'             => array(
+                'type'    => 'Segment',
                 'options' => array(
-                    'route' => '/irc[/]',
+                    'route'    => '/irc[/]',
                     'defaults' => array(
                         '__NAMESPACE__' => 'PageController\Controller',
                         'controller'    => 'Page',
@@ -97,9 +97,9 @@ return array(
                     ),
                 ),
             ),
-            'about' => array(
-                'type' => 'Zend\Mvc\Router\Http\Segment',
-                'options' => array(
+            'about'           => array(
+                'type'          => 'Segment',
+                'options'       => array(
                     'route'    => '/about[/]',
                     'defaults' => array(
                         '__NAMESPACE__' => 'PageController\Controller',
@@ -109,32 +109,32 @@ return array(
                 ),
                 'may_terminate' => true,
                 'child_routes'  => array(
-                    'faq' => array(
-                        'type' => 'Zend\Mvc\Router\Http\Segment',
-                        'options' => array(
+                    'faq'    => array(
+                        'type'          => 'Segment',
+                        'options'       => array(
                             'route'    => 'faq[/]',
                             'defaults' => array(
-                                'page'          => 'about/faq',
+                                'page' => 'about/faq',
                             ),
                         ),
                         'may_terminate' => true,
                     ),
                     'faq-v1' => array(
-                        'type' => 'Zend\Mvc\Router\Http\Segment',
-                        'options' => array(
+                        'type'          => 'Segment',
+                        'options'       => array(
                             'route'    => 'faq-v1[/]',
                             'defaults' => array(
-                                'page'          => 'about/faq-v1',
+                                'page' => 'about/faq-v1',
                             ),
                         ),
                         'may_terminate' => true,
                     ),
                 ),
             ),
-            'license' => array(
-                'type' => 'Segment',
-                'options' => array(
-                    'route' => '/license[/]',
+            'license'         => array(
+                'type'          => 'Segment',
+                'options'       => array(
+                    'route'    => '/license[/]',
                     'defaults' => array(
                         '__NAMESPACE__' => 'PageController\Controller',
                         'controller'    => 'Page',
@@ -142,12 +142,23 @@ return array(
                     ),
                 ),
                 'may_terminate' => true,
-                'child_routes' => array(
+                'child_routes'  => array(
                     'new-bsd' => array(
-                        'type' => 'Literal',
+                        'type'    => 'Literal',
                         'options' => array(
                             'route' => 'new-bsd',
                         ),
+                    ),
+                ),
+            ),
+            'zend-config-xml' => array(
+                'type'    => 'Segment',
+                'options' => array(
+                    'route'    => '/xml/zend-config-xml/1.0[/]',
+                    'defaults' => array(
+                        '__NAMESPACE__' => 'PageController\Controller',
+                        'controller'    => 'Page',
+                        'page'          => 'zend-config-xml-1-0',
                     ),
                 ),
             ),
