d089281257995a2396169043fe7dbc2f1df3ca17
Matthew Weier O'Phinney <matthew@zend.com>
2014-11-24 15:46:54 -0600

    Ensure manual defaults to latest minor version
    - Added an application-level config file that will set the default version for
      the manual route.
    - Added a target to Makefile to update the above file based on the version
      specified; this target gets invoked when we prepare the site for a release.
    - Updated the redirects to pull the version from the above config file if
      /manual or /manual/ are requested.
    - Added a "serve" target to Makefile to make serving the site via the built-in
      web server simpler; allows passing a PORT variable if desired.

---
+++ config/autoload/zf-manual-routes.global.php
@@ -0,0 +1,19 @@
+<?php
+return array (
+  'router' => 
+  array (
+    'routes' => 
+    array (
+      'manual' => 
+      array (
+        'options' => 
+        array (
+          'defaults' => 
+          array (
+            'version' => '2.3',
+          ),
+        ),
+      ),
+    ),
+  ),
+);
