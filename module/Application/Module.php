162561d341646b51ebb9d88d239738de642974bf
Frank Brückner <dev@froschdesignstudio.de>
2015-11-27 13:58:33 +0100

    Removes getAutoloaderConfig method in all module classes

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -25,17 +25,6 @@ class Module implements ConsoleUsageProviderInterface
         return include __DIR__ . '/config/module.config.php';
     }
 
-    public function getAutoloaderConfig()
-    {
-        return array(
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                ),
-            ),
-        );
-    }
-
     public function getControllerConfig()
     {
         return array('factories' => array(
--- module/Sitemap/Module.php
+++ module/Sitemap/Module.php
@@ -4,17 +4,6 @@ namespace Sitemap;
 
 class Module
 {
-    public function getAutoloaderConfig()
-    {
-        return array(
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                ),
-            ),
-        );
-    }
-
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
