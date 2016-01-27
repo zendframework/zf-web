6d0928189dafd14c39085544288d415560c372c2
Matthew Weier O'Phinney <matthew@zend.com>
2016-01-27 17:24:02 -0600

    Redirect /expressive to Expressive site
    Implements a `RedirectListener` that will take routed requests and
    redirect them elsewhere, based on configuration. This is then used to
    re-route the `/expressive` route to the Expressive site.

---
+++ config/autoload/redirects.global.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'redirects' => [
+        'expressive' => [
+            'target' => 'https://zendframework.github.io/zend-expressive/',
+            'status' => 301,
+        ],
+    ],
+];
